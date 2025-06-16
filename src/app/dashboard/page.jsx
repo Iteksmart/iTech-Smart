891c4b376eb7bdcdfaf4dc291c976dabddd3a1d7
Iteksmart <53306844+Iteksmart@users.noreply.github.com>
2025-06-15 21:32:04 -0400

    Update page.jsx

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useHandleStreamResponse } from "../utilities/runtime-helpers";
+import { useHandleStreamResponse } from "../utilities/runtime-helpers1";
 
 
 function MainComponent() {
